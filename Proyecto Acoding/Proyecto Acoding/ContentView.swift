48c8bc0bbf9073009539f94ba166b8465f242090
José Luis <joseluiscorrallopez@Joses-MacBook-Air.local>
2025-12-15 14:42:16 +0100

    Proyect Creation

---
+++ Proyecto Acoding/Proyecto Acoding/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Proyecto Acoding
+//
+//  Created by José Luis Corral López on 15/12/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Proyecto Acoding/Proyecto Acoding/Proyecto_AcodingApp.swift
@@ -0,0 +1,17 @@
+//
+//  Proyecto_AcodingApp.swift
+//  Proyecto Acoding
+//
+//  Created by José Luis Corral López on 15/12/25.
+//
+
+import SwiftUI
+
+@main
+struct Proyecto_AcodingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
